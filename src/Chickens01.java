9fcb998859c002dcc1f012c7a25a12fee2ea6207
ebGinblat <elias.ginblat@stu.ocsb.ca>
2022-10-14 14:38:11 -0400

    answer one

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -2,6 +2,20 @@ public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
         
-        System.out.println(totalEggs);
+    	int totalEggs = 0;
+    	int chickenCount = 4;
+    	int eggsPerChicken = 8;       
+        //Monday
+    	totalEggs=totalEggs+chickenCount*eggsPerChicken;
+    	
+    	//Tuesday
+    	chickenCount++;
+    	totalEggs+=chickenCount*eggsPerChicken;
+    	
+    	//Wednesday
+    	chickenCount/=2;
+    	totalEggs+=chickenCount*eggsPerChicken;
+    	System.out.println(totalEggs);
+        
     }   
 }
